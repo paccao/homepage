1e8dee584171f2e53bca6bbfd8151d00a640e7cf
paccao <16489683+paccao@users.noreply.github.com>
2026-04-05 11:09:03 +0200

    Update consts.ts with site information

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,3 @@
-// Place any global data in this file.
-// You can import this data from anywhere in your site by using the `import` keyword.
-
-export const SITE_TITLE = 'Astro Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = "Joel Plumppu";
+export const SITE_DESCRIPTION =
+    "Welcome to my website where I write about projects I work on and other things that I find interesting.";

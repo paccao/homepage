cff8438095461634c389d00dac2c89cdaecf785e
paccao <16489683+paccao@users.noreply.github.com>
2026-05-05 18:49:48 +0200

    Fix paths for github pages, including rss

--- src/consts.ts
+++ src/consts.ts
@@ -1,3 +1,6 @@
 export const SITE_TITLE = "Joel Plumppu";
 export const SITE_DESCRIPTION =
     "Welcome to my website where I write about projects I'm building and other things that I find interesting.";
+export const isDev = import.meta.env.DEV === true;
+const PAGE_PREFIX = "/homepage";
+export const GITHUB_PAGES_PATH_PREFIX = isDev ? "" : PAGE_PREFIX;

4dee6d273211bd180bb5089080b15f54190c3448
paccao <16489683+paccao@users.noreply.github.com>
2026-05-05 19:44:52 +0200

    Attempt 1 to fix base path dev & prod

--- src/consts.ts
+++ src/consts.ts
@@ -1,6 +1,6 @@
 export const SITE_TITLE = "Joel Plumppu";
 export const SITE_DESCRIPTION =
     "Welcome to my website where I write about projects I'm building and other things that I find interesting.";
-export const isDev = import.meta.env.DEV === true;
-const PAGE_PREFIX = "/homepage";
-export const GITHUB_PAGES_PATH_PREFIX = isDev ? "" : PAGE_PREFIX;
+export const isDev = import.meta.env.DEV ?? false;
+const GH_PAGE_PREFIX = "/homepage";
+export const GITHUB_PAGES_PATH_PREFIX = isDev === true ? "" : GH_PAGE_PREFIX;

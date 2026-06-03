44554d01ad397d003b9bc39fbd0e6d262f8e1763
Joel Plumppu <16489683+paccao@users.noreply.github.com>
2026-06-03 13:28:07 +0200

    Change site domain, remove gh pages prefix
    update site links to / as root instead of /homepage

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,10 @@
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
-import { GITHUB_PAGES_PATH_PREFIX } from "./src/consts";
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://paccao.github.io" + GITHUB_PAGES_PATH_PREFIX,
-    base: GITHUB_PAGES_PATH_PREFIX,
-    integrations: [mdx(), sitemap()],
+  site: "https://joel.plumppu.se",
+  base: "",
+  integrations: [mdx(), sitemap()],
 });

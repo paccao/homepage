270bf8f3dcd426c6c48831400092b7307e12a557
paccao <16489683+paccao@users.noreply.github.com>
2026-09-04 11:53:53 +0200

    Installed tailwind and tw typography

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,15 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import { defineConfig } from "astro/config";
 
+import tailwindcss from "@tailwindcss/vite";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://joel.plumppu.se",
   base: "",
   integrations: [mdx(), sitemap()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
